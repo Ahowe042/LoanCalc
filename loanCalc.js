4c5bba35273ab24c42d271c0b62e5c6be3d4b6ab
Ahowe042 <ahowe042@gmail.com>
2018-06-27 13:25:11 -0400

    Add Input Checks
    Added checks to input, runs only if numbers are truly numerical. Also checks  for commas and refuses them, as modern day JavaScript accepts those as decimals. Localization might require removing that check later

--- loanCalc.js
+++ loanCalc.js
@@ -1,3 +1,4 @@
+"use strict";
 function runCalc() {
 	var textField = document.getElementById("textField");
 	let pPrinciple = document.getElementById('princText').value;
@@ -9,13 +10,16 @@ function runCalc() {
 	let intPmt = parseFloat(pMonthlyPayment);
 	let monthsToPay = 0;
 	let yearsToPay = 0;
-	
+
+	if (!checkInputs(pPrinciple, pRate, pMonthlyPayment)) {
+		alert("Please enter valid numberical values, without dollar signs, percentage marks, commas, or other characters.");
+	} else {
 	intRate = intRate / 100.0;
 	textField.value = intPrinc;
 
-	
+
 	// Calculates amount of time in months/years needed to pay off loan with specific payment amount.
-	// Calculation based on common percentage rates drawn by banks and loan institutions ~ interest is added 
+	// Calculation based on common percentage rates drawn by banks and loan institutions ~ interest is added
 	// on a daily basis, therefore interest rate must be divided by 365, then multiplied by average days per month
 	while (intPrinc > 0)
 	{
@@ -29,11 +33,12 @@ function runCalc() {
 		}
 		monthsToPay = monthsToPay + 1;
 		yearsToPay = monthsToPay / 12;
-    	textField.value += ", " + intPrinc;
-    	document.getElementById("output").innerHTML = "It will take " + monthsToPay + " months to pay off the loan, or " + yearsToPay.toFixed(1) + " years.";
+		if (intPrinc > 0) {
+			textField.value += ", " + intPrinc.toFixed(2);
+		}
 	}
-
-	
+	document.getElementById("output").innerHTML = "It will take " + monthsToPay + " months to pay off the loan, or " + yearsToPay.toFixed(1) + " years.";
+}
 }
 
 function clearFields() {
@@ -46,4 +51,27 @@ function clearFields() {
 	intstText.value = "";
 	mnthPmtText.value = "";
 	document.getElementById("output").innerHTML = "";
+}
+
+function checkInputs(pPrinciple, pRate, pMonthlyPayment) {
+	let inputCheck = false;
+
+	if (isNaN(parseFloat(pPrinciple)) || isNaN(parseFloat(pRate)) || isNaN(parseFloat(pMonthlyPayment))) {
+    inputCheck = false;
+  } else {
+		if ((pPrinciple.indexOf(",") < 0) && (pRate.indexOf(",") < 0) && (pMonthlyPayment.indexOf(",") < 0)) {
+    	inputCheck = true;
+		}
+  }
+
+	return inputCheck;
+}
+
+// Allows 'Enter' key to trigger 'Run Calculator' button
+document.onkeydown = function (e) {
+  e = e || window.event;
+  switch (e.which || e.keyCode) {
+        case 13 : runCalc();
+            break;
+  }
 }
